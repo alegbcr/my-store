4c8689659c3d4e31b8f1ab100e13e0e121c4f8bf
Alexander González B <alexgbcr@proton.me>
2023-06-15 22:11:18 -0600

    [UPDATE] change file home

--- api/routes/home.router.js
+++ api/routes/home.router.js
@@ -2,9 +2,6 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// Servir archivos estáticos
-router.use(express.static('public'));
-
 // Home
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
